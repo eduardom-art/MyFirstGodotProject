f1d5ef7ad2c462c0466e7215e4471f437c89494e
eduardom-art <eduardomoises3110@gmail.com>
2026-09-05 20:49:50 -0600

    Godot
    We added a simple jumping system to the character.
    velocity_y controls the character's vertical speed.
    gravity makes the character fall down.
    jump_force makes the character move upward.
    ground defines where the floor is.
    is_on_ground tells us if the character is touching the floor.
    is_action_just_pressed() detects when the jump key is pressed.
    The and condition makes sure the character can only jump while on the ground.
    
    The character now jumps upward, slows down, falls because of gravity, and stops when it reaches the floor.

--- icon.gd
+++ icon.gd
@@ -1,11 +1,28 @@
 extends Sprite2D
 
+var velocity_y = 0
+var gravity = 20
+var jumping_power = -500
+var floor = 400
+var on_the_floor = true
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		position.x += 5
+
 	if Input.is_action_pressed("ui_left"):
 		position.x -= 5
-	if Input.is_action_pressed("ui_down"):
-		position.y += 5
-	if Input.is_action_pressed("ui_up"):
-		position.y -= 5
+
+	velocity_y += gravity
+	position.y += velocity_y
+
+	if position.y >= floor:
+		position.y = floor
+		velocity_y = 0
+		on_the_floor = true
+	else:
+		on_the_floor = false
+		
+	if Input.is_action_just_pressed("ui_up") and on_the_floor:
+		velocity_y = jumping_power
+		on_the_floor = false
